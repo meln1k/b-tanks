2e6897de6644ae7bd3fd2314f405d43817d69072
Sergey Azovskov <last-g@yandex-team.ru>
2015-07-16 22:16:15 +0500

    Real build

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,19 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var mainBowerFiles = require('gulp-main-bower-files');
 
-gulp.task('bower', function() {
+gulp.task('bower-install', function() {
+
+    return bower({cmd: 'update'});
+});
+
+gulp.task('bower-compile', ['bower-install'], function() {
+    bower();
     return gulp.src('./bower.json')
         .pipe(mainBowerFiles())
         .pipe(gulp.dest('./libs'))
 });
 
 
-gulp.task('default', ['bower'], function() {
+gulp.task('default', ['bower-compile'], function() {
     app = require('./bin/www');
 });
